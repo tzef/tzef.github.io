23e1178315522bc86c001310ddf293e0e10af16a
LEE ZHE YU <tzef8220@gmail.com>
2016-05-30 15:41:35 +0800

    init

---
+++ products/smalltrade_files/grayscale.js
@@ -0,0 +1,12 @@
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.ms-screens a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
